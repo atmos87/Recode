585629f808cb08260c205f9419418b5a1b997382
Dino Constantinou <dconstantinou360@gmail.com>
2022-06-14 12:07:32 +0100

    Initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,20 +3,30 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `RECODE`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://recode.limited`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "pages"
-  }]
+    {
+      resolve: 'gatsby-plugin-theme-ui',
+      options: {
+        prismPreset: 'night-owl',
+        preset: '@theme-ui/preset-funk',
+      },
+    }
+  ]
 };
 
 export default config;
